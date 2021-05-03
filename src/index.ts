d4f93e785e5462076e7c3d1b428f5f5f03b17268
Alaric Whitehead <alaric.whitehead@gmail.com>
2021-05-03 14:50:36 +0100

    feat: switched to using the @whide/tree-lang package to provide a more general binary tree parser

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
 //Connectors
 export { HWhileConnector, HWhileConnectorProps } from "./HwhileConnector";
 export { InteractiveHWhileConnector, ProgramInfo, RunResultType, StepResultType } from "./InteractiveConnector";
-
-//Parsers
-export { default as parseTree, BinaryTree } from "./parsers/TreeParser";
-
-//Converters
-export * as IntegerTreeConverter from "./converters/IntegerTreeConverter";
-export * as TreeListConverter from "./converters/TreeListConverter";
-export * as NumberListConverter from "./converters/NumberListConverter";
-export * as TreeStringConverter from "./converters/TreeStringConverter";
