7b6f78dd3b1bded0a89e1526712b029746345e79
sonrad10 <alaric.whitehead@gmail.com>
2021-11-18 16:53:10 +0000

    feat: made state monitoring commands (e.g. :store) have their output hidden by default

--- src/InteractiveConnector.ts
+++ src/InteractiveConnector.ts
@@ -18,7 +18,19 @@ export type RunResultType = { cause: 'breakpoint'; line: number; }
 		| { cause: 'done'; variable: string; value: BinaryTree };
 
 /**
- * Class for controlling HWhile's interactive mode
+ * Class for controlling HWhile's interactive mode.
+ *
+ * The output string produced by HWhile is returned through events.
+ * Subscribe to the {@code 'output'} channel to receive the output.
+ *
+ * Each function call has 2 boolean parameters (named {@code output} and {@code outputState}) which control which
+ * function's results are written to the output.
+ * The {@code output = true} parameter describes whether this command (and its output result) should be displayed in the output.
+ *
+ * Additionally, the {@code outputState = false} parameter describes whether any subsequent calls (and their respective outputs)
+ * to maintain the program state should be outputted. These include calls to check for breakpoint locations, and the
+ * current value of variables.
+ * Not all functions contain these calls, so {@code outputState} may not affect the functionality.
  */
 export class InteractiveHWhileConnector extends EventEmitter {
 	private _hWhileConnector: HWhileConnector;
@@ -38,7 +50,7 @@ export class InteractiveHWhileConnector extends EventEmitter {
 	}
 
 	/**
-	 * Start a HWhile interactive process
+	 * Start an HWhile interactive process.
  	 */
 	public async start() : Promise<void> {
 		this._shell = this._hWhileConnector.interactive();
@@ -84,7 +96,9 @@ export class InteractiveHWhileConnector extends EventEmitter {
 	}
 
 	/**
-	 *
+	 * The shell process (if started) which is running the HWhile process.
+	 * @returns {ChildProcessWithoutNullStreams}	If the process is started
+	 * @returns {undefined}	Otherwise
 	 */
 	get shell(): ChildProcessWithoutNullStreams | undefined {
 		return this._shell;
@@ -96,17 +110,19 @@ export class InteractiveHWhileConnector extends EventEmitter {
 
 	/**
 	 * Evaluate a while expression or run a command.
-	 * USE WITH CAUTION.
-	 * @param expr		String to evaluate
-	 * @param output	Whether to emit the HWhile output string to listeners
-	 * @returns	The string outputted by hwhile to stdout while running, split into lines
+	 * It is recommended this is used with caution as it may affect the internal state of this connector object.
+	 * @param expr			String to evaluate
+	 * @param output		Whether to emit the HWhile output string to listeners
+	 * @param outputState	Whether to emit the HWhile state commands output string to listeners
+	 * @returns {string[]}	The string outputted by hwhile to stdout while running, split into lines
 	 */
-	async execute(expr: string, output = true): Promise<string[]> {
+	async execute(expr: string, output = true, outputState = false): Promise<string[]> {
 		return new Promise((resolve, reject) => {
 			if (!this._shell) {
 				reject("No shell to access. Try calling `this.start()`.");
 				return;
 			}
+			//Remove leading/trailing whitespace characters
 			expr = expr.trim();
 
 			this._dataCallbacks.push((data: string[], str: string) => {
@@ -115,7 +131,7 @@ export class InteractiveHWhileConnector extends EventEmitter {
 			});
 			//Write the command to the output
 			this._outputHolder.push(expr);
-			this.emit('output', expr + '\n');
+			if (output) this.emit('output', expr + '\n');
 			this._shell.stdin.write(expr + '\n');
 		})
 	}
@@ -123,9 +139,10 @@ export class InteractiveHWhileConnector extends EventEmitter {
 	/**
 	 * Print the help message.
 	 * @param output	Whether to emit the HWhile output string to listeners
+	 * @param outputState	Whether to emit the HWhile state commands output string to listeners
 	 */
-	async help(output = true) : Promise<void> {
-		await this.execute(':help', output);
+	async help(output = true, outputState = false) : Promise<void> {
+		await this.execute(':help', output, outputState);
 	}
 
 	/**
@@ -133,14 +150,16 @@ export class InteractiveHWhileConnector extends EventEmitter {
 	 * Note that this clears the current store contents.
 	 * @param p		The program to load.
 	 * 				For a program 'p', this will load from the file 'p.while' in the current directory.
-	 * @param expr	Argument to provide to the program {@code p}..
-	 * @param output	Whether to emit the HWhile output string to listeners
+	 * @param expr			Argument to provide to the program {@code p}..
+	 * @param output		Whether to emit the HWhile output string to listeners
+	 * @param outputState	Whether to emit the HWhile state commands output string to listeners
 	 */
-	async load(p : string, expr : string, output = true) : Promise<ProgramInfo> {
+	async load(p : string, expr : string, output = true, outputState = false) : Promise<ProgramInfo> {
 		//Run the command
-		let lines : string[] = await this.execute(`:load ${p} ${expr}`, output);
+		let lines : string[] = await this.execute(`:load ${p} ${expr}`, output, outputState);
 		//Remove the command string
 		lines.shift();
+		//Get the response to the load command
 		let result = lines.shift();
 
 		if (result && result.match(/^Program '(.+)' loaded/)) {
@@ -150,7 +169,7 @@ export class InteractiveHWhileConnector extends EventEmitter {
 				breakpoints: [],
 			};
 			// Check for existing breakpoints
-			let breakpoints: CustomDict<Set<number>> = await this.breakpoints(output);
+			let breakpoints: CustomDict<Set<number>> = await this.breakpoints(outputState);
 			if (breakpoints[p]) this._programInfo.breakpoints = Array.from(breakpoints[p]);
 			return this._programInfo;
 		} else {
@@ -161,12 +180,14 @@ export class InteractiveHWhileConnector extends EventEmitter {
 	/**
 	 * Run the loaded program up until the next breakpoint, or the end of the program (whichever is first)..
 	 * @param output	Whether to emit the HWhile output string to listeners
+	 * @param outputState	Whether to emit the HWhile state commands output string to listeners
+	 * @returns {RunResultType}	A state object describing the state of the program at the point that the execution stops.
 	 */
-	async run(output = true) : Promise<RunResultType> {
+	async run(output = true, outputState = false) : Promise<RunResultType> {
 		if (!this._programInfo) throw new Error("No program to run");
 
 		//Run the program
-		let lines: string[] = await this.execute(':run', output);
+		let lines: string[] = await this.execute(':run', output, outputState);
 
 		//Remove the command string
 		lines.shift();
@@ -207,20 +228,22 @@ export class InteractiveHWhileConnector extends EventEmitter {
 		}
 
 		//Update the stored variables
-		await this.store(false);
+		await this.store(outputState);
 
 		return result;
 	}
 
 	/**
 	 * Step through a single line of the loaded program..
-	 * @param output	Whether to emit the HWhile output string to listeners
+	 * @param output		Whether to emit the HWhile output string to listeners
+	 * @param outputState	Whether to emit the HWhile state commands output string to listeners
+	 * @returns {StepResultType}	A state object describing the state of the program after the next line has finished executing.
 	 */
-	async step(output = true) : Promise<StepResultType> {
+	async step(output = true, outputState = false) : Promise<StepResultType> {
 		if (!this._programInfo) throw new Error("No program to run");
 
 		//Run the next line
-		let lines : string[]  = await this.execute(`:step`, output);
+		let lines : string[]  = await this.execute(`:step`, output, outputState);
 
 		//Remove the command string
 		lines.shift();
@@ -265,7 +288,7 @@ export class InteractiveHWhileConnector extends EventEmitter {
 		}
 
 		//Update the stored variables
-		await this.store(false);
+		await this.store(outputState);
 
 		return result;
 	}
@@ -274,11 +297,12 @@ export class InteractiveHWhileConnector extends EventEmitter {
 	 * Get the current store contents.
 	 * Returns a dictionary of dictionaries, first indexed by program, then by variable.
 	 * Also updates the stored variable values for the loaded program.
-	 * @param output	Whether to emit the HWhile output string to listeners
-	 * @returns		A mapping of programs to a map of variables to values..
+	 * @param output		Whether to emit the HWhile output string to listeners
+	 * @param outputState	Whether to emit the HWhile state commands output string to listeners
+	 * @returns {Map<string,Map<string,string>>}	A mapping of programs to a mapping of variables to values.
 	 */
-	async store(output = true) : Promise<Map<string, Map<string, BinaryTree>>> {
-		let lines = await this.execute(`:store`, output);
+	async store(output = true, outputState = false) : Promise<Map<string, Map<string, BinaryTree>>> {
+		let lines = await this.execute(`:store`, output, outputState);
 		const variables : Map<string, Map<string, BinaryTree>> = new Map();
 
 		//Get all the lines matching the output format "(prog) VARIABLE = [OUTPUT_VAL]"
@@ -305,38 +329,46 @@ export class InteractiveHWhileConnector extends EventEmitter {
 	 * @param dir		The directory to switch to.
 	 * 					This must be an absolute path..
 	 * @param output	Whether to emit the HWhile output string to listeners
+	 * @param outputState	Whether to emit the HWhile state commands output string to listeners
 	 */
-	async cd(dir: string, output = true) : Promise<void> {
-		await this.execute(`:cd ${dir}`, output);
+	async cd(dir: string, output = true, outputState = false) : Promise<void> {
+		await this.execute(`:cd ${dir}`, output, outputState);
 	}
 
 	/**
-	 * Get all the breakpoints.
+	 * Get all the breakpoints currently active in the program.
+	 * @param output		Whether to emit the HWhile output string to listeners
+	 * @param outputState	Whether to emit the HWhile state commands output string to listeners
 	 */
-	async breakpoints(output = true) : Promise<CustomDict<Set<number>>> {
-		let lines = await this.execute(`:break`, output);
+	async breakpoints(output = true, outputState = false) : Promise<CustomDict<Set<number>>> {
+		//Execute the break command and waits for the output
+		let lines = await this.execute(`:break`, output, outputState);
+		//Get all the lines describing a breakpoint
 		let matches = this._runMatch(lines, /^Program '(.+)', line (\d+)\.$/);
+		//Convert the list of strings to the return type
 		let breakpoints: CustomDict<Set<number>> = {};
 		for (let match of matches) {
-			let p: string = match[1];
-			let b: Set<number> = breakpoints[p] || new Set<number>();
-			b.add(Number.parseInt(match[2]));
-			breakpoints[p] = b;
+			//The program name
+			let prog: string = match[1];
+			//Save the breakpoint under the name of its program
+			breakpoints[prog] = breakpoints[prog] || new Set<number>();
+			breakpoints[prog].add(Number.parseInt(match[2]));
 		}
 		return breakpoints;
 	}
 
 	/**
 	 * Add a breakpoint on line 'n' of the program 'p', or the loaded program if `p` is not provided.
-	 * @param n	Line number to add the break point
-	 * @param p	Optional program to add the breakpoint to.
-	 * @param output	Whether to emit the HWhile output string to listeners
+	 * @param n				Line number to add the break point
+	 * @param p				Optional program to add the breakpoint to.
+	 * @param output		Whether to emit the HWhile output string to listeners
+	 * @param outputState	Whether to emit the HWhile state commands output string to listeners
 	 */
-	async addBreakpoint(n: number, p?: string, output = true) : Promise<void> {
+	async addBreakpoint(n: number, p?: string, output = true, outputState = false) : Promise<void> {
 		//Error if HWhile won't be able to set the breakpoint
 		if (!p && !this._programInfo) throw new Error("Can't set breakpoints without a program. Please load a program, or specify one explicitly.");
 
-		let lines: string[] = await this.execute(`:break ${n} ${p || ''}`, output);
+		let lines: string[] = await this.execute(`:break ${n} ${p || ''}`, output, outputState);
 		//Remove the command string
 		lines.shift();
 		let last = lines.shift();
@@ -347,15 +379,16 @@ export class InteractiveHWhileConnector extends EventEmitter {
 
 	/**
 	 * Delete the breakpoint on line 'n' of the program 'p', or the loaded program if `p` is not provided.
-	 * @param n	Line number to remove the break point
-	 * @param p	Optional program to remove the breakpoint from.
-	 * @param output	Whether to emit the HWhile output string to listeners
+	 * @param n				Line number to remove the break point
+	 * @param p				Optional program to remove the breakpoint from.
+	 * @param output		Whether to emit the HWhile output string to listeners
+	 * @param outputState	Whether to emit the HWhile state commands output string to listeners
 	 */
-	async delBreakpoint(n: number, p?: string, output = true) : Promise<void> {
+	async delBreakpoint(n: number, p?: string, output = true, outputState = false) : Promise<void> {
 		//Error if HWhile won't be able to clear the breakpoint
 		if (!p && !this._programInfo) throw new Error("Can't set breakpoints without a program. Please load a program, or specify one explicitly.");
 
-		let lines: string[] = await this.execute(`:delbreak ${n} ${p || ''}`, output);
+		let lines: string[] = await this.execute(`:delbreak ${n} ${p || ''}`, output, outputState);
 		//Remove the command string
 		lines.shift();
 		let last = lines.shift();
